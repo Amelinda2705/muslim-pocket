cdc0c91cc1d8bb290a9479bafce9102b411c2593
Amelinda <amelindaa2705@gmail.com>
2024-02-05 11:48:06 +0700

    initialize git

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:muslimpocket/home/tracker.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    HomePage homePage = const HomePage();
+    MaterialApp materialApp = MaterialApp(
+      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
+      home: homePage,
+    );
+
+    return materialApp;
+  }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+// }
+}
