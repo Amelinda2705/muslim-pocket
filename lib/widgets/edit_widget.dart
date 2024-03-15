b4683d6845ab114fc8cf21d3292f71b30ff439da
Amelinda <amelindaa2705@gmail.com>
2024-03-15 09:50:46 +0700

    add edit tracker

--- lib/widgets/edit_widget.dart
+++ lib/widgets/edit_widget.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:muslimpocket/commons/global.dart';
+import 'package:muslimpocket/firebase_auth_implementation/firebase_auth_services.dart';
+import 'package:muslimpocket/widgets/wrapper_widget.dart';
 
 class EditPage extends StatefulWidget {
   const EditPage({super.key});
@@ -12,6 +15,9 @@ class EditPage extends StatefulWidget {
 class _EditPageState extends State<EditPage> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
@@ -141,132 +147,160 @@ class _EditPageState extends State<EditPage> {
             ],
           ),
         ),
-        Column(
-          children: [
-            Container(
-              width: 237.0,
-              height: 34.0,
-              margin: const EdgeInsets.only(top: 5.0),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20.0,
-              ),
-              decoration: BoxDecoration(
-                  color: Global().bgDone,
-                  borderRadius: BorderRadius.circular(30.0)),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Subuh',
-                      style: TextStyle(
-                        fontWeight: Global().medium,
+        SizedBox(
+          height: height * .3,
+          width: width * 07,
+          child: WrapperWidget(
+            builder: (user) {
+              if (user == null) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const LoginOrRegister()),
+                    );
+                  },
+                  child: const Text("Login"),
+                );
+              }
+              DateTime date = DateTime.now();
+              return Container(
+                color: Global().bgBlur,
+                child: Column(
+                  children: [
+                    Center(
+                      child:
+                          StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                        stream: FirebaseFirestore.instance
+                            .collection("trackers")
+                            .doc(
+                                "${user.uid}-${date.year}${date.month}${date.day}")
+                            .snapshots(),
+                        builder: (_, snapshot) {
+                          double width = MediaQuery.of(context).size.width;
+                          double height = MediaQuery.of(context).size.height;
+
+                          if (!snapshot.hasData) {
+                            return const Text("Loading");
+                          }
+                          if (snapshot.data == null ||
+                              snapshot.data!.data() == null) {
+                            WidgetsBinding.instance
+                                .addPostFrameCallback((timeStamp) {
+                              FirebaseFirestore.instance
+                                  .collection("trackers")
+                                  .doc(
+                                      "${user.uid}-${date.year}${date.month}${date.day}")
+                                  .set({
+                                "userId": user.uid,
+                                "createdAt": FieldValue.serverTimestamp(),
+                                "checklists": [
+                                  "Subuh",
+                                  "Zuhur",
+                                  "Asar",
+                                  "Magrib",
+                                  "Isya",
+                                ]
+                              });
+                            });
+                            return const Text("Tidak ada data");
+                            // Bikin data disini
+                          }
+                          DocumentSnapshot<Map<String, dynamic>> doc =
+                              snapshot.data!;
+                          return Column(
+                            children: List.generate(
+                                doc.data()!['checklists'].length, (index) {
+                              String value = doc.data()!['checklists'][index];
+                              bool isCheck = false;
+                              try {
+                                isCheck = doc.data()!['udahChecklists'][index];
+                              } catch (e) {}
+                              return Column(
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      List<bool> value = [];
+                                      try {
+                                        value = List<bool>.from(
+                                            doc.data()!['udahChecklists']);
+                                      } catch (e) {
+                                        print(e);
+                                        value = List.generate(
+                                            doc.data()!['checklists'].length,
+                                            (index) => false);
+                                      }
+                                      value[index] = !isCheck;
+                                      doc.reference.update({
+                                        "udahChecklists": value,
+                                        "asdh": ""
+                                      });
+                                    },
+                                    child: Container(
+                                      width: width * .6,
+                                      height: height * .04,
+                                      margin: const EdgeInsets.only(top: 5.0),
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: 20.0,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: Global().bgDone,
+                                        borderRadius:
+                                            BorderRadius.circular(30.0),
+                                      ),
+                                      child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: <Widget>[
+                                            Expanded(
+                                                child: Text(
+                                              value,
+                                              style: TextStyle(
+                                                fontWeight: Global().medium,
+                                              ),
+                                            )),
+                                            FaIcon(
+                                              FontAwesomeIcons.bars,
+                                              size: width * .05,
+                                            ),
+                                          ]),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }),
+                          );
+                        },
                       ),
                     ),
-                    Image.asset(
-                      'assets/images/edit.png',
-                      width: 15.0,
-                      height: 12.0,
-                    )
-                  ]),
-            ),
-            Container(
-              width: 237.0,
-              height: 34.0,
-              margin: const EdgeInsets.only(top: 5.0),
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              decoration: BoxDecoration(
-                  color: Global().bgDone,
-                  borderRadius: BorderRadius.circular(30.0)),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Zuhur',
-                      style: TextStyle(
-                        fontWeight: Global().medium,
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: width * .6,
+                        height: height * .04,
+                        margin: const EdgeInsets.only(top: 5.0),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Global().greenPrimary,
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        child: Center(
+                          child: FaIcon(
+                            FontAwesomeIcons.plus,
+                            size: width * .05,
+                            color: Global().white,
+                          ),
+                        ),
                       ),
-                    ),
-                    Image.asset(
-                      'assets/images/edit.png',
-                      width: 15.0,
-                      height: 12.0,
                     )
-                  ]),
-            ),
-            Container(
-              width: 237.0,
-              height: 34.0,
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              margin: const EdgeInsets.only(top: 5.0),
-              decoration: BoxDecoration(
-                  color: Global().bgNotYet,
-                  borderRadius: BorderRadius.circular(30.0)),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Asar',
-                      style: TextStyle(
-                        fontWeight: Global().medium,
-                      ),
-                    ),
-                    Image.asset(
-                      'assets/images/edit.png',
-                      width: 15.0,
-                      height: 12.0,
-                    )
-                  ]),
-            ),
-            Container(
-              width: 237.0,
-              height: 34.0,
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              margin: const EdgeInsets.only(top: 5.0),
-              decoration: BoxDecoration(
-                  color: Global().bgNotYet,
-                  borderRadius: BorderRadius.circular(30.0)),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Magrib',
-                      style: TextStyle(
-                        fontWeight: Global().medium,
-                      ),
-                    ),
-                    Image.asset(
-                      'assets/images/edit.png',
-                      width: 15.0,
-                      height: 12.0,
-                    )
-                  ]),
-            ),
-            Container(
-              width: 237.0,
-              height: 34.0,
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              margin: const EdgeInsets.only(top: 5.0),
-              decoration: BoxDecoration(
-                  color: Global().bgNotYet,
-                  borderRadius: BorderRadius.circular(30.0)),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Isya',
-                      style: TextStyle(
-                        fontWeight: Global().medium,
-                      ),
-                    ),
-                    Image.asset(
-                      'assets/images/edit.png',
-                      width: 15.0,
-                      height: 12.0,
-                    )
-                  ]),
-            ),
-            AddPray("", ""),
-          ],
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ],
     );
